e6e451bbd3bab95fbdf25430120a577c6e91015d
zishanx <zishanalikhan53@gmail.com>
2026-07-03 21:53:54 +0530

    Prime number

--- STL/learning.js
+++ STL/learning.js
@@ -1,20 +1,34 @@
-let n = 36
-let sol = []
+// Prime number :- the number that has exactly two factors 1 and itself
 
-for (let i = 1; i <= Math.sqrt(n); i++) {
+let n = 6
+
+let count = 0
+/*
+for (let i = 1; i <= n; i++) {
     if (n % i === 0) {
+        count++
+    }
+}
 
-        sol.push(i)
-        if (n / i !== i) {
-            sol.push(n / i)
+if (count === 2) {
+    console.log("it's a prime number")
+} else {
+    console.log("it's not a prime number.")
+}
+*/
+// TL = O(N)
+
+for (let i = 1; i * i <= n; i++) {
+    if(n % i === 0 ){
+        count ++
+        if( n / i !== i ){
+            count ++
         }
     }
 }
-// O(n logn): n is the number of factor.
-sol.sort((a,b)=> a-b)
 
-
-// O(number of factor)
-sol.forEach(element => {
-    console.log(element)
-});
+if(count == 2 ){
+    console.log("it's prime")
+}else {
+    console.log("it's not a prime")
+}
