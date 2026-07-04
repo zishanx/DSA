cb067ccf1834f17a82720a18135d5bd10677444c
zishanx <zishanalikhan53@gmail.com>
2026-07-04 11:29:14 +0530

    Done with the GCD

--- STL/learning.js
+++ STL/learning.js
@@ -1,34 +1,31 @@
-// Prime number :- the number that has exactly two factors 1 and itself
+// Euclidean Algo
 
-let n = 6
+/* 
+GCD(n1, n2) = gcd(n1-n2, n2)  where n1 > n2
 
-let count = 0
-/*
-for (let i = 1; i <= n; i++) {
-    if (n % i === 0) {
-        count++
-    }
-}
 
-if (count === 2) {
-    console.log("it's a prime number")
-} else {
-    console.log("it's not a prime number.")
-}
+gcd (a,b) = gcd(a % b , b) where a > b 
+
+greater % smaller till one of them is zero when one of them is zero the other is gcd .
 */
-// TL = O(N)
 
-for (let i = 1; i * i <= n; i++) {
-    if(n % i === 0 ){
-        count ++
-        if( n / i !== i ){
-            count ++
-        }
+let a = 50
+
+let b = 100
+
+while (a > 0 && b > 0) {
+    if (a > b) {
+        a = a % b
+    } else {
+        b = b % a
     }
+
+
+}
+if (a === 0) {
+    console.log("GCD is " + b)
+} else {
+    console.log("GCD is " + a)
 }
 
-if(count == 2 ){
-    console.log("it's prime")
-}else {
-    console.log("it's not a prime")
-}
+// TC = O(log fi(min(a,b)))
