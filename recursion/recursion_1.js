57549b2ba8653936a39ff778f7ee4f196ca711a3
zishanx <zishanalikhan53@gmail.com>
2026-07-07 12:00:00 +0530

    Your commit message

--- recursion/recursion_1.js
+++ recursion/recursion_1.js
@@ -1,6 +1,26 @@
-function f(){
-    console.log(1)
-    f()
+/* 
+Recursion:- Recursion is when a function calls itself .until a condition is met .
+
+
+The condition to stop it is called a base condition.
+
+There can be single based condition and multiple based condition.
+
+
+Recursion tree.
+The call of function one by one by one . all those funciton make a recursion tree 
+*/
+
+let count = 0 
+
+
+function f() {
+    if(count === 3 ){
+        return
+    } // Here this if is the base condition . 
+    console.log(count)
+    count++
+    f() 
 }
 
 f()
