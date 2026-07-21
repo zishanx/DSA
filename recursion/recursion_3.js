df800ebe858228dc6aee381e5ec6dd561e148303
zishanx <zishanalikhan53@gmail.com>
2026-07-21 08:40:59 +0530

    Reverse brute force

--- recursion/recursion_3.js
+++ recursion/recursion_3.js
@@ -2,12 +2,20 @@
 
 class solution {
 
-    reverseArray(arr,l,r) {
+    reverseArray(arr, l, r) {
 
-        if(l >= r ) return
+        if (l >= r) {return console.log(arr)}
 
-        arr[0] = arr[]
-        
+        [arr[l], arr[r]] = [arr[r], arr[l]];
+
+        this.reverseArray(arr, l + 1, r - 1)
+
     }
 
 }
+
+let a = [1, 2, 3, 4, 5, 2, 1]
+
+const sol =   new solution
+
+sol.reverseArray(a, 0, a.length - 1)
