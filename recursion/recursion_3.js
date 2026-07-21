137d18d446c101de38dc70f919c6a0fc1ed7228e
zishanx <zishanalikhan53@gmail.com>
2026-07-21 09:14:23 +0530

    did one var reverse

--- recursion/recursion_3.js
+++ recursion/recursion_3.js
@@ -2,9 +2,12 @@
 
 class solution {
 
+
+    // two var 
+
     reverseArray(arr, l, r) {
 
-        if (l >= r) {return console.log(arr)}
+        if (l >= r) { return console.log(arr) }
 
         [arr[l], arr[r]] = [arr[r], arr[l]];
 
@@ -12,10 +15,23 @@ class solution {
 
     }
 
+    // with a single var
+
+    rev(i, arr) {
+        let n = arr.length
+
+        if (i >= n / 2) { return console.log(arr) }
+
+        [arr[i], arr[n - i - 1]] = [arr[n - i - 1], arr[i]];
+
+        this.rev(i + 1, arr)
+    }
+
 }
 
 let a = [1, 2, 3, 4, 5, 2, 1]
 
-const sol =   new solution
+const sol = new solution
 
-sol.reverseArray(a, 0, a.length - 1)
+// sol.reverseArray(a, 0, a.length - 1)
+sol.rev(0, a)
