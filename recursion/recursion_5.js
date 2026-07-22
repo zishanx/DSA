9d7df98d98ee08519714bec8e73ecb81b44c4d2a
zishanx <zishanalikhan53@gmail.com>
2026-07-22 11:39:59 +0530

    for loop done

--- recursion/recursion_5.js
+++ recursion/recursion_5.js
@@ -2,11 +2,21 @@ class Solution {
 
     // trying it with for loops
     fibonacci(n) {
+        let first = 0;
+        let second = 1;
+        let fib = 0
 
-        
+
+
+        for (let i = 0; i < n; i++) {
+            console.log(first)
+            fib = first + second;
+            [first, second] = [second, fib]
+        }
+
     }
 }
 
 const sol = new Solution()
 
-sol.fibonacci(0, 4, 0)
+sol.fibonacci(4)
