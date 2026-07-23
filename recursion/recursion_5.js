80835ef2dc2565b7d03cfa5ce8c50b707d7536ec
zishanx <zishanalikhan53@gmail.com>
2026-07-23 10:49:10 +0530

    Done with the fibonacci

--- recursion/recursion_5.js
+++ recursion/recursion_5.js
@@ -1,22 +1,30 @@
+
+// Fibonnaci we know that fib(i) = fib(i-1) + fib(1-2) and i guess we will have to remember it too . 
+
 class Solution {
 
     // trying it with for loops
     fibonacci(n) {
-        let first = 0;
-        let second = 1;
-        let fib = 0
-
-
-
-        for (let i = 0; i < n; i++) {
-            console.log(first)
-            fib = first + second;
-            [first, second] = [second, fib]
+        
+        let fib = [0,1]
+        for(let i = 2; i < n ; i++){
+            fib.push(i-1 + i-2);
         }
+        console.log(fib[n])
 
+    }
+
+    fib(n) {
+        if (n <= 1) return n
+
+        let last = this.fib(n - 1)
+        let sLast = this.fib(n - 2)
+
+        return last + sLast
     }
 }
 
 const sol = new Solution()
 
 sol.fibonacci(4)
+console.log(sol.fib(4))
