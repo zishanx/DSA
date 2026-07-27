25adf9cd7ce88d71a67fe62aec24e458a68e0cfa
zishanx <zishanalikhan53@gmail.com>
2026-07-27 10:40:02 +0530

    brute force

--- hashing/hashing_1.js
+++ hashing/hashing_1.js
@@ -1,15 +1,16 @@
-let arr = [1,2,1,3,2]
+let arr = [1, 2, 1, 3, 2]
 
 
-function  hash (number , array){
-    
-    let count = 0 
+function hash(number, array) {
 
-    for(let i = 0 ; i < array.length ; i++){
-        if(array[i]=== 1) count ++
+    let count = 0
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === number) { count++ }
+
     }
 
-    console.log(count)
+    return count
 }
 
-hash (1, arr)
+console.log(hash(19, arr))
