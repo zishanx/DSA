8d0530fcbb8e9a9658f24aabc5ba540695fa7c74
zishanx <zishanalikhan53@gmail.com>
2026-07-27 11:17:23 +0530

    Done

--- hashing/hashing_1.js
+++ hashing/hashing_1.js
@@ -1,10 +1,14 @@
 
-let arr = [2,1,2,4,4,2,5,8,7,2,9,3]
+let arr = [2, 1, 2, 4, 4, 2, 5, 8, 7, 2, 9, 3]
 
-let hash = new Array(1000000).fill(0)
+let hash = new Array(13).fill(0)
 
-for(let i = 0 ; i < 13 - 1;i ++){
+for (let i = 0; i < 13 - 1; i++) {
     hash[arr[i]] += 1
 }
 
-console.log(hash)
+function finding(number) {
+    return hash[number]
+}
+
+console.log(finding(6))
