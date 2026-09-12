fdbcc3d49152c104683650a60d2a6f619ac5184e
zishanx <zishanalikhan53@gmail.com>
2026-09-12 11:54:56 +0530

    Count

--- Maths/digits.js
+++ Maths/digits.js
@@ -2,16 +2,28 @@
 
 // Firstly let's say we have a digit 99789. What we can do first is modulo of that digit and we will get the last digit . Now we can divide it by 10 and take only the integer part of it using Math.floor() and then do this till n becomes 0 . we will have extracted each digit in reverse order. 
 
-class Solution 
+class Solution {
     extract = (n) => {
         while (n > 0) {
             let d = n % 10;
             console.log(d);
-            d = Math.floor(d/10)
+            n = Math.floor(n / 10)
         }
     }
 
+    count = (n) => {
+        let count = 0
+
+        while (n > 0) {
+
+            count++;
+            n = Math.floor(n / 10)
+
+        }
+        console.log(count)
+    }
+}
 
 let sol = new Solution()
 
-sol.extract(99789);
+sol.count(99789);
