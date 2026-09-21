98d48add7ada3383a155c2c0e45dfc6caf744a4c
zishanx <zishanalikhan53@gmail.com>
2026-09-21 09:28:55 +0530

    Done with the prime

--- Maths/prime.js
+++ Maths/prime.js
@@ -15,8 +15,28 @@ class Solution {
 
         // TL Big O(n)
     }
+
+    // working with sqroot method 
+
+    opti(n) {
+        let count = 0;
+        for (let i = 0; i * i < n; i++) {
+            if (n % i === 0) {
+                count++
+                if (n / i !== i) {
+                    count++
+                }
+            }
+        }
+        if (count === 2) {
+            console.log("it's a prime ")
+        } else {
+            console.log("it's not a prime.")
+        }
+    }
 }
 
 const sol = new Solution()
 
-sol.brute(2)
+// sol.brute(2)
+sol.opti(15)
