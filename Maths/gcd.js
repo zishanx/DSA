a25154e3b75834a6c9f257fe0d460a096427c243
zishanx <zishanalikhan53@gmail.com>
2026-09-21 10:40:20 +0530

    Done with the basic maths

--- Maths/gcd.js
+++ Maths/gcd.js
@@ -4,15 +4,36 @@
 class Solution {
     brute(x, y) {
         let largest = 0;
-        for (let i = 0; i <= Math.max(x, y); i++) {
+        for (let i = 0; i <= Math.min(x, y); i++) {
             if (x % i === 0 && y % i === 0) {
                 largest = i
             }
         }
         console.log(largest)
+        // >> TL Big O(max(x,y))
+    }
+
+
+    euclid(x, y) {
+        // >>Euclidean- The algo go on like GCD(n1,n2) = GCD(n1-n2,n2) where n1 > n2 
+
+
+        while (x > 0 & y > 0) {
+            if (x > y){
+                x = x % y
+            }else{
+                y = y % x
+            }
+        }
+
+        console.log(Math.max(x, y))
+
+
     }
 }
 
 const sol = new Solution()
 
-sol.brute(5, 53)
+sol.brute(5, 53)  
+
+sol.euclid(5,55)
