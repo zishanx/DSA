71d29117ae03f25ab1c9e24393a6003493ba768c
zishanx <zishanalikhan53@gmail.com>
2026-09-25 11:18:56 +0530

    reversing an arr

--- recursion/recursion_3.js
+++ recursion/recursion_3.js
@@ -1,37 +1,19 @@
-//Reverse a give Array
-
-class solution {
+// reverse an Array 
 
 
-    // two var 
-
-    reverseArray(arr, l, r) {
-
-        if (l >= r) { return console.log(arr) }
-
-        [arr[l], arr[r]] = [arr[r], arr[l]];
-
-        this.reverseArray(arr, l + 1, r - 1)
-
+class Solution {
+    reverse(a, l, r) {
+        if (l >= r) return a;
+        [a[l], a[r]] = [a[r], a[l]]
+        return this.reverse(a, l + 1, r - 1)
     }
-
-    // with a single var
-
-    rev(i, arr) {
-        let n = arr.length
-
-        if (i >= n / 2) { return console.log(arr) }
-
-        [arr[i], arr[n - i - 1]] = [arr[n - i - 1], arr[i]];
-
-        this.rev(i + 1, arr)
-    }
-
 }
 
-let a = [1, 2, 3, 4, 5, 2, 1]
 
-const sol = new solution
+const sol = new Solution()
 
-// sol.reverseArray(a, 0, a.length - 1)
-sol.rev(0, a)
+let arr = [1, 23, 4, 7, 9]
+let l = 0;
+let r = arr.length - 1
+
+console.log(sol.reverse(arr, l, r))
