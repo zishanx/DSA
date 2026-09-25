0c8f5d6a75cbb6e0ddd5b26b458e795538c68312
zishanx <zishanalikhan53@gmail.com>
2026-09-25 11:37:39 +0530

    Working with the single para reverse

--- recursion/recursion_3.js
+++ recursion/recursion_3.js
@@ -7,6 +7,16 @@ class Solution {
         [a[l], a[r]] = [a[r], a[l]]
         return this.reverse(a, l + 1, r - 1)
     }
+
+    // with a single para
+    revSing(arr, i) {
+        let n = arr.length
+        if (i >= n / 2) return arr;
+
+        [arr[i], arr[n - i - 1]] = [arr[n - i - 1], arr[i]]
+
+        return this.revSing(arr, i + 1)
+    }
 }
 
 
@@ -16,4 +26,6 @@ let arr = [1, 23, 4, 7, 9]
 let l = 0;
 let r = arr.length - 1
 
-console.log(sol.reverse(arr, l, r))
+// console.log(sol.reverse(arr, l, r))
+
+console.log(sol.revSing(arr, l))
